0ecab7db833f67456241244cd42bc26f53986726
Anh Tuan Truong <atruon16@student.aau.dk>
2020-02-10 13:06:30 +0100

    Add project files.

---
+++ Object Oriented Programming/School.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Object_Oriented_Programming
+{
+    class School
+    {
+        public string schoolName;
+        public int schoolStudents;
+        public int schoolProfessors;
+
+        public string GetName()
+        {
+            return $"{ schoolName }";
+        }
+    }
+}
