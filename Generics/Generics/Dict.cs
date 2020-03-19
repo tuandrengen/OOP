ea6f563889f2bd3d33d53c7e15f8473fac25179a
Anh Tuan Truong <atruon16@student.aau.dk>
2020-03-19 21:41:25 +0100

    Generics - ex3 missing

---
+++ Generics/Generics/Dict.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class Dict<K, V>
+    {
+        List<K> keys = new List<K>();
+        public K Get<K>(K key)
+        {
+            foreach (var item in keys)
+            {
+                if (item.Equals(key))
+                {
+                    return key;
+                }
+            }
+            throw new Exception("Key not found.");
+        }
+        void Put<K,V>(K key, V value)
+        {
+
+        }
+    }
+}
